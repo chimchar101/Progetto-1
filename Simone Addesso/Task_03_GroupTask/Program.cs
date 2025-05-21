33e81323359cb67abf7a63de1e83c94af3526840
Simone Addesso <simoneaddesso25@gmail.com>
2025-05-21 17:41:07 +0200

    Add files via upload

---
+++ Simone Addesso/Task_03_GroupTask/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+public class Corso
+{
+    public string NomeCorso, Docente;
+    public int DurataOre;
+    public List<string> Studenti;
+
+    public Corso(string nomeCorso, string docente, int durataOre, List<string> studenti)
+    {
+        NomeCorso = nomeCorso;
+        Docente = docente;
+        DurataOre = durataOre;
+        Studenti = studenti;
+    }
+
+    public void AggiungiStudente(string nomeStudente) // FATTO
+    {
+        Studenti.Add(nomeStudente);
+    }
+
+    public virtual string ToString()
+    {
+        return $"Nome corso: {NomeCorso} | Durata ore: {DurataOre} | Docente: {Docente} | Studenti: {Studenti}";
+    }
+
+    public virtual void MetodoSpeciale()
+    {
+        Console.WriteLine("Ciao campione");
+    }
+}
+
+public class CorsoDanza : Corso
+{
+    public string Stile;
+    public CorsoDanza(string nomeCorso, string docente, int durataOre, List<string> studenti, string stile) : base(nomeCorso, docente, durataOre, studenti)
+    {
+        Stile = stile;
+    }
+
+    
+    public override string ToString()
+    {
+        return $"Nome corso: {NomeCorso} | Durata ore: {DurataOre} | Docente: {Docente} | Studenti: {Studenti} | Stile: {Stile}";
+    }
+
+    public override void MetodoSpeciale()
+    {
+        Console.WriteLine($"Esecuzione coreografia nello stile: {Stile}");
+    }
+}
