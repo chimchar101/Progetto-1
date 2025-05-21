42107bf9139ade0962e0b3d95d923379a0a53673
carlocond <carlocondello05@gmail.com>
2025-05-21 17:39:59 +0200

    Add files via upload
    +Scrittura della classe CorsoMusica

---
+++ Progetto1 Carlo/Task_03_GroupTask/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+public class Corso
+{
+    public string NomeCorso;
+    public int DurataOre;
+    public int Docente;
+
+    public List<string> Studenti;
+
+    public Corso(string nomeCorso, int durataOre, int docente, List<string> studenti)
+    {
+        NomeCorso = nomeCorso;
+        DurataOre = durataOre;
+        Docente = docente;
+        Studenti = studenti;
+    }
+
+    public void AggiungiStudente(string nomeStudente) //FATTO
+    {
+        Studenti.Add(nomeStudente);
+    }
+    public virtual string toString()
+    {
+        return $"Nome corso: {NomeCorso} | Durata ore: {DurataOre} | Docente: {Docente} | Studenti {Studenti}";
+    }
+
+    public virtual void MetodoSpeciale()
+    {
+        Console.WriteLine("Ciao campione");
+    }
+}
+
+public class CorsoMusica : Corso
+{
+    public string Strumento;
+
+    public CorsoMusica(string nomeCorso, int durataOre, int docente, List<string> studenti, string strumento) : base(nomeCorso, durataOre, docente, studenti)
+    {
+        Strumento = strumento;
+    }
+
+    public override string toString()
+    {
+        return $"Nome corso: {NomeCorso} | Durata ore: {DurataOre} | Docente: {Docente} | Studenti {Studenti} | Strumento utilizzato: {Strumento}";
+    }
+
+    public override void MetodoSpeciale()
+    {
+        Console.WriteLine($"Si tiene una prova pratica dello strumento: {Strumento}");
+    }
+}
